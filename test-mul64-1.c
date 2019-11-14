c80a46be9d655222246150f893fbac1f7cd32681
Bo-Yin Yang <by@crypto.tw>
2019-11-15 03:58:29 +0800

    school book radix 2^19

---
+++ test-mul64-1.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include "polymul_16x16.h"
+//#include "cmsis.h"
+
+#define WARMCACHE 0
+#define NUMTESTS  10
+#define QQ 4591
+
+
+//extern unsigned long long cpucycles(void);
+//extern __m256i montproduct(__m256i x,__m256i y);
+
+//void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
+//int cmpfunc (const void * a, const void * b) {
+//   return ( *(int*)a - *(int*)b );
+//}
+
+//extern void montmult_16x16(__m256i * zl, __m256i *zh , __m256i x , __m256i y);
+//extern int jump16divsteps (int delta, __m256i *fi, __m256i *gi, __m256i *uvqr);
+//int times[NUMTESTS];
+
+void rand16 (short *b) {
+  int i;
+  for (i=0; i<16; i++) {
+    b[i] = rand() % QQ;
+    if (b[i] > QQ/2) b[i]-=QQ;
+  }
+}
+
+void rand16_1 (short *b) {
+  int i;
+  for (i=0; i<16; i++) {
+    b[i] = rand() % 3 - 1;
+  }
+}
+
+void print8 (int *b) {
+  int i,y;
+  y = b[0];
+  printf("((%d)+(%d)*x",(int)(short) y, (y>>16));
+  for (i=1; i<4; i++) {
+    y = b[i];
+    printf("+(%d)*x^%d+(%d)*x^%d",(int)(short)y,2*i,(y>>16),2*i+1);
+  }
+  printf(")");
+}
+
+// void print8 (short *b) {
+//   int i;
+//   printf("((%d)+(%d)*x",b[0],b[1]);
+//   for (i=2; i<8; i++) {
+//     printf("+(%d)*x^%d",b[i],i);
+//   }
+//   printf(")");
+// }
+  
+
+int main (void) {
+  int i, delta;
+  int ff[32], gg[32], hh[64];
+  short *f = (short *)ff;
+  short *g = (short *)gg;
+  short *h = (short *)hh;
+  //unsigned long long cycles, time1, time2;
+  //int c,j;
+  
+  //cycles = 0;
+  for (i=0; i< NUMTESTS + WARMCACHE ; i++) { 
+
+    rand16(f); rand16(f+16); rand16(f+32); rand16(f+48);
+    rand16_1(g); rand16_1(g+16); rand16_1(g+32); rand16_1(g+48);
+  
+    printf("f=GF4591x(");
+    print8(ff);
+    printf("+\nx^8*");
+    print8(ff+4);
+    printf("+\nx^16*");
+    print8(ff+8);
+    printf("+\nx^24*");
+    print8(ff+12);
+    printf("+\nx^32*");
+    print8(ff+16);
+    printf("+\nx^40*");
+    print8(ff+20);
+    printf("+\nx^48*");
+    print8(ff+24);
+    printf("+\nx^56*");
+    print8(ff+28);
+    printf(")\ng=GF4591x(");
+    print8(gg);
+    printf("+\nx^8*");
+    print8(gg+4);
+    printf("+\nx^16*");
+    print8(gg+8);
+    printf("+\nx^24*");
+    print8(gg+12);
+    printf("+\nx^32*");
+    print8(gg+16);
+    printf("+\nx^40*");
+    print8(gg+20);
+    printf("+\nx^48*");
+    print8(gg+24);
+    printf("+\nx^56*");
+    print8(gg+28);
+    printf(")\n");
+    
+    //time1  = cpucycles();
+    gf_polymul_NxN(hh,ff,gg,64,32);
+    //time2  = cpucycles();
+    printf("h = GF4591x(");
+    print8(hh);
+    printf("+\nx^8*");
+    print8(hh+4);
+    printf("+\nx^16*");
+    print8(hh+8);
+    printf("+\nx^24*");
+    print8(hh+12);
+    printf("+\nx^32*");
+    print8(hh+16);
+    printf("+\nx^40*");
+    print8(hh+20);
+    printf("+\nx^48*");
+    print8(hh+24);
+    printf("+\nx^56*");
+    print8(hh+28);
+    printf("+\nx^64*");
+    print8(hh+32);
+    printf("+\nx^72*");
+    print8(hh+36);
+    printf("+\nx^80*");
+    print8(hh+40);
+    printf("+\nx^88*");
+    print8(hh+44);
+    printf("+\nx^96*");
+    print8(hh+48);
+    printf("+\nx^104*");
+    print8(hh+52);
+    printf("+\nx^112*");
+    print8(hh+56);
+    printf("+\nx^120*");
+    print8(hh+60);
+    printf(")\n");
+    
+    if (i>= WARMCACHE) {
+      //cycles += (time2 - time1);
+      //printf("%lld ",time2-time1);
+      //times[i-WARMCACHE] = time2-time1;
+    }
+  }
+  //printf("%lld cycles ",(cycles)/NUMTESTS);
+  //qsort(times, NUMTESTS, sizeof(int), cmpfunc);
+  //printf("%d cycles median\n",times[NUMTESTS/2]);
+  //printf("fails %d\n",fail);
+  return(0);
+}
