dfa32ffb6ea59761d874b10d342aa95cb4e29138
B.Y <by@crypto.tw>
2019-12-01 22:02:22 +0800

    negacyclic schoolbook

---
+++ polymul_NxN_negc.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+import sys
+import re
+from math import log,ceil,floor,sqrt
+
+q = 4591
+qinv = 15631	# q^{-1} mod 2^16
+q16inv = 14	# round(2^16/q)
+q32inv = 935519	# round(2^32/q)
+ARGS = sys.argv
+SAVES = 0
+if ("BARRETT" in sys.argv) : MONT_OR_BAR = 1
+else : MONT_OR_BAR = 0
+
+try :
+    NN = int(sys.argv[1])
+except :
+    NN = 64
+    
+def alloc_save (S) : # allocate variable name s_S as a space
+    global SAVES
+    if not (("s_"+S) in locals() or ("s_"+S) in globals()) :
+        globals()["s_"+S] = "s" + str(SAVES)
+        SAVES +=1
+    
+# NARGS = len(ARGS)
+# if (NARGS == 1) :
+#     aux = open("polymul_NxN_aux.h","w+")
+    
+# adjust
+# def adj_size (size) :
+#     if (size < q_ab) :  # 2341 is q-dependent
+#         return(size)
+#     else :
+#         i = ceil ((floor(size * q16 / 2.0**16 + 0.5) - 0.5) * 2.0**16 / q16) - 1
+#         return(i - q * floor(i * q16 / 2.0**16 +0.5))
+
+# may change to using vmov later
+def print_ldr (reg, loc, comment) :
+    #print("	ldr	%s, %s	// %s" % (reg, loc, comment))
+    print("	vmov	%s, %s  // %s" % (reg, loc, comment))
+
+def print_str (reg, loc, comment) :
+    #print("	str	%s, %s	// %s" % (reg, loc, comment))
+    print("	vmov	%s, %s	// %s" % (loc, reg, comment))
+    
+alloc_save("mq") # modulus -q = -4591
+alloc_save("qi") # q^{-1} mod 2^16 or
+alloc_save("q32")# round(2^32/q)
+alloc_save("h")  # save h
+alloc_save("0")  # scratch 0
+alloc_save("1")  # scratch 1
+alloc_save("2")  # scratch 2
+alloc_save("3")  # scratch 3
+
+def SCH_negc_prologue (N) :
+    print '#include "red-asm.h"'
+    print "	.p2align	2,,3	"
+    print "	.syntax		unified"
+    print "	.text"
+    if (MONT_OR_BAR == 0) :
+        print "// void gf_polymul_%dx%d_negc_divR (int32_t *h, int32_t *f, int32_t *g);" % (N,N)
+        print "	.global gf_polymul_%dx%d_negc_divR" % (N,N)
+        print "	.type	gf_polymul_%dx%d_negc_divR, %%function" % (N,N)
+        print "gf_polymul_%dx%d_negc_divR:" % (N,N)
+    else :
+        print "// void gf_polymul_%dx%d_negc (int32_t *h, int32_t *f, int32_t *g);" % (N,N)
+        print "	.global gf_polymul_%dx%d_negc" % (N,N)
+        print "	.type	gf_polymul_%dx%d_negc, %%function" % (N,N)
+        print "gf_polymul_%dx%d_negc:" % (N,N)
+    
+    print "	push	{r4-r11,lr}"
+    # r1, r2 = multiplicand pointers to f and g
+    # r3, r4 = multiplicand f, r12, r14 = multiplicand g
+    # print_str("r0",s_h,"save h")
+    if (MONT_OR_BAR == 0) :
+        print "	movw	r14, #%d" % (qinv)
+        print_str("r14", s_qi, "save qinv")
+    else :
+        print "	movw	r14, #%d" % (q32inv % 65536) 
+        print "	movt	r14, #%d" % (q32inv // 65536)
+        print_str("r14", s_q32, "save q32inv")
+    print "	movw	r12, #%d" % (65536-q)
+    print "	movt	r12, #65535"
+    print_str("r12", s_mq, "save -q")
+
+def SCH_negc_epilogue() :
+    print "	pop	{r4-r11,lr}"
+    print "	bx	lr"
+
+
+from polymul_NxN_negc_i import SCH_polymulNxN_negc
+
+# if (NARGS > 1) :
+
+if (MONT_OR_BAR == 0) : s = s_qi
+else : s = s_q32
+    
+SCH_negc_prologue(NN)
+SCH_polymulNxN_negc(NN,"r1","r2","r0",s_mq,s,s_0,s_1,s_2,s_3,MONT_OR_BAR)
+SCH_negc_epilogue()
---
+++ polymul_NxN_negc_i.py
@@ -0,0 +1,181 @@
+import sys
+import re
+from math import log,ceil,floor,sqrt
+
+q = 4591
+qinv = 15631	# q^{-1} mod 2^16
+q16inv = 14	# round(2^16/q)
+q32inv = 935519	# round(2^32/q)
+SAVES = 0
+global MONT_OR_BAR 
+
+r_f = "r1"
+r_g = "r2"
+r_h = "r0"
+
+# rotating accumulator k during round i
+def acc_r (i,k) :
+    return("r%d" % (5+(4*i+k)%7)) 
+
+def is_even (E) :
+    if isinstance(E,int) :
+        return (int(E/2)*2 == E)
+    else :
+        return(False)
+
+def alloc_save (S) : # allocate variable name s_S as a space
+    global SAVES
+    if not (("s_"+S) in locals() or ("s_"+S) in globals()) :
+        globals()["s_"+S] = "s" + str(SAVES)
+        SAVES +=1
+
+alloc_save("mq") # modulus -q = -4591
+alloc_save("qi") # q^{-1} mod 2^16 or
+alloc_save("q32")# round(2^32/q)
+alloc_save("h")  # save h
+alloc_save("0")  # scratch 0
+alloc_save("1")  # scratch 1
+alloc_save("2")  # scratch 2
+alloc_save("3")  # scratch 3
+
+# may change to using vmov later
+def print_ldr (reg, loc, comment) :
+    #print("	ldr	%s, %s	// %s" % (reg, loc, comment))
+    print("	vmov	%s, %s  // %s" % (reg, loc, comment))
+
+def print_str (reg, loc, comment) :
+    #print("	str	%s, %s	// %s" % (reg, loc, comment))
+    print("	vmov	%s, %s	// %s" % (loc, reg, comment))
+
+# multiply and accumulate block pointed to by a and b (in row a+b)
+def add_block(a,b,i) :
+    print "block_%d_%d:" % (a,b)
+    print "	ldr	r3, [%s, #%d]" % (r_f,8*a)
+    print "	ldr	r4, [%s, #%d]" % (r_f,8*a+4)
+    print "	ldr	r12, [%s, #%d]" % (r_g,8*b)
+    print "	ldr	r14, [%s, #%d]" % (r_g,8*b+4)
+    print "	smladx	%s, r3, r12, %s" % (acc_r(i,1),acc_r(i,1))
+    print "	smladx	%s, r4, r12, %s" % (acc_r(i,3),acc_r(i,3))
+    print "	smladx	%s, r3, r14, %s" % (acc_r(i,3),acc_r(i,3))
+    print "	smladx	%s, r4, r14, %s" % (acc_r(i,5),acc_r(i,5))
+    print "	smlabb	%s, r3, r12, %s" % (acc_r(i,0),acc_r(i,0))
+    print "	smlabb	%s, r3, r14, %s" % (acc_r(i,2),acc_r(i,2))
+    print "	pkhtb	r3, r3, r4"
+    print "	smlad	%s, r3, r12, %s" % (acc_r(i,2),acc_r(i,2))
+    print "	smlad	%s, r3, r14, %s" % (acc_r(i,4),acc_r(i,4))
+    print "	smlatt	%s, r4, r12, %s" % (acc_r(i,4),acc_r(i,4))
+    print "	smlatt	%s, r4, r14, %s" % (acc_r(i,6),acc_r(i,6))
+    
+# first block of row i, r3, r4, r12, r14 already set up
+def add_block_first(i) :
+    print "	smladx	%s, r3, r12, %s" % (acc_r(i,1),acc_r(i,1))
+    print "	smuadx	%s, r4, r12" % (acc_r(i,3))
+    print "	smladx	%s, r3, r14, %s" % (acc_r(i,3),acc_r(i,3))
+    print "	smuadx	%s, r4, r14" % (acc_r(i,5))
+    print "	smlabb	%s, r3, r12, %s" % (acc_r(i,0),acc_r(i,0))
+    print "	smlabb	%s, r3, r14, %s" % (acc_r(i,2),acc_r(i,2))
+    print "	pkhtb	r3, r3, r4"
+    print "	smlad	%s, r3, r12, %s" % (acc_r(i,2),acc_r(i,2))
+    print "	smuad	%s, r3, r14" % (acc_r(i,4))
+    print "	smlatt	%s, r4, r12, %s" % (acc_r(i,4),acc_r(i,4))
+    print "	smultt	%s, r4, r14" % (acc_r(i,6))
+   
+def reduce_4acc (i, scr) :
+    print_ldr("r3",s_mq,"load -q")
+    if (MONT_OR_BAR == 0) :
+        print_ldr("r12",s_qi,"load qinv")
+        redm = "mr_16x2"
+    else :
+        print_ldr("r12",s_q32,"load q32inv")
+        redm = "br_32x2"
+    print "	%s	%s, %s, r3, r12, %s" % (redm,acc_r(i,0),acc_r(i,1),scr)
+    print "	%s	%s, %s, r3, r12, %s" % (redm,acc_r(i,2),acc_r(i,3),scr)
+    print "	str	%s, [%s], #4" % (acc_r(i,0),r_h)
+    print "	str	%s, [%s], #4" % (acc_r(i,2),r_h)
+
+def SCH_polymulNxN_negc(N,rf,rg,rh,smq,sqi,s0,s1,s2,s3,MB) :
+    assert (N == int(N/4)*4)    
+    global r_f, r_g, r_h, s_mq, s_qi, s_q32, s_0, s_1, s_2, s_3, MONT_OR_BAR
+    MONT_OR_BAR = MB;
+    r_f = rf; r_g = rg; r_h = rh; s_mq = smq;
+    s_0 = s0; s_1 = s1; s_2 = s2; s_3 = s_3
+    if (MONT_OR_BAR == 0) : s_qi = sqi
+    else : s_q32 = sqi
+    
+    # load 4x4 multiplications at a time from (r3,r4), (r12,r14)
+    # doing 7 rotating accumulators in r5-r11
+    print "	ldr	r3, [%s, #%d]" % (r_f, 2*N-8)
+    print "	ldr	r4, [%s, #%d]" % (r_f, 2*N-4)
+    print "	ldr	r12, [%s]" % (r_g)
+    print "	ldr	r14, [%s, #4]" % (r_g)
+    # left top, very first block
+    print "block_%d_0:" % (N/4-1)
+    print "	smuadx	%s, r3, r12" % (acc_r(-1,1))
+    print "	smuadx	%s, r4, r12" % (acc_r(-1,3))
+    print "	smladx	%s, r3, r14, %s" % (acc_r(-1,3),acc_r(-1,3))
+    print "	smuadx	%s, r4, r14" % (acc_r(-1,5))
+    print "	smulbb	%s, r3, r12" % (acc_r(-1,0))
+    print "	smulbb	%s, r3, r14" % (acc_r(-1,2))
+    print "	pkhtb	r3, r3, r4"
+    print "	smlad	%s, r3, r12, %s" % (acc_r(-1,2),acc_r(-1,2))
+    print "	smuad	%s, r3, r14" % (acc_r(-1,4))
+    print "	smlatt	%s, r4, r12, %s" % (acc_r(-1,4),acc_r(-1,4))
+    print "	smultt	%s, r4, r14" % (acc_r(-1,6))
+    for b in range(1,N/4) :
+        a = N/4 - 1 - b
+        add_block(a, b, -1)
+    print_str(acc_r(-1,0),s_0,"save thread 0")
+    print_str(acc_r(-1,1),s_1,"save thread 1")
+    print_str(acc_r(-1,2),s_2,"save thread 2")
+    print_str(acc_r(-1,3),s_3,"save thread 3")
+    
+    for i in range(N/4-1) :
+        a += 1
+        print "	ldr	r3, [%s, #%d]" % (r_f,8*a)
+        print "	ldr	r4, [%s, #%d]" % (r_f,8*a+4)
+        if (i>0) : print "	ldr	r12, [%s, #%d]" % (r_g,8*b)
+        print "block_%d_%d:" % (a,b)
+        add_block_first(i)
+        #
+        # split even and odd i
+        #
+        if (is_even(i)) : # even i, already done (i+1, N/4-1)
+            for a in range(i+2, N/4) :
+            	b = N/4 + i - a # until a = N/4 = 0, b = i
+                add_block(a, b, i)
+        else : # odd i, already done (i,0)
+            for b in range(1, i+1) :
+                a = i - b # until a = 0, b = i
+                add_block(a, b, i);
+        print "	neg	%s, %s" % (acc_r(i,0), acc_r(i,0))
+        print "	neg	%s, %s" % (acc_r(i,1), acc_r(i,1))
+        print "	neg	%s, %s" % (acc_r(i,2), acc_r(i,2))
+        print "	neg	%s, %s" % (acc_r(i,3), acc_r(i,3))
+        print "	neg	%s, %s" % (acc_r(i,4), acc_r(i,4))
+        print "	neg	%s, %s" % (acc_r(i,5), acc_r(i,5))
+        print "	neg	%s, %s" % (acc_r(i,6), acc_r(i,6))
+
+        if (is_even(i)) : # even i, a = 0, b = i
+            for a in range(i+1) : # until a = i, b = 0
+                b = i - a
+                add_block(a, b, i);
+        else : # odd i, a = N/4 - 1, b = i+1
+            for b in range(i+1, N/4) : # until a = i+1, b = N/4 - 1
+                a = N/4 + i - b
+                add_block(a, b, i);
+            print "	neg	%s, %s" % (acc_r(i,0), acc_r(i,0)) 
+            print "	neg	%s, %s" % (acc_r(i,1), acc_r(i,1)) 
+            print "	neg	%s, %s" % (acc_r(i,2), acc_r(i,2)) 
+            print "	neg	%s, %s" % (acc_r(i,3), acc_r(i,3)) 
+        reduce_4acc(i,"r4")
+    # after very last block, need to reduce the last 4 accumulators
+    print_ldr(acc_r(N/4-2,1),s_0,"load thread 0")
+    print_ldr(acc_r(N/4-2,2),s_1,"load thread 1")
+    print_ldr(acc_r(N/4-2,3),s_2,"load thread 2")
+    print_ldr(acc_r(N/4-2,0),s_3,"load thread 3")
+    print "	add	%s, %s" % (acc_r(N/4-2,4),acc_r(N/4-2,1))
+    print "	add	%s, %s" % (acc_r(N/4-2,5),acc_r(N/4-2,2))
+    print "	add	%s, %s" % (acc_r(N/4-2,6),acc_r(N/4-2,3))
+    reduce_4acc(N/4-1,"r4")
+    print "	add	%s, #%d" % (r_f,2*N)
+    print "	add	%s, #%d" % (r_g,2*N)
