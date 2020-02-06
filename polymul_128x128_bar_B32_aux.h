161c960a316137aaf444294bd82a2c52ea88a501
B.Y <by@crypto.tw>
2020-02-07 07:17:02 +0800

    stuff

---
+++ polymul_128x128_bar_B32_aux.h
@@ -0,0 +1,175 @@
+	.p2align	2,,3
+	.syntax		unified
+	.text
+
+KA_exp_ov_128:
+KA_exp_ov_128_128:
+	.hword	-1
+KA_exp_add_128_128:
+	.hword	32	// #TERMS(128,128)/4
+KA_exp_ov_128_64:
+	.hword	-1
+KA_exp_add_128_64:
+	.hword	48	// #TERMS(128,64)/4
+KA_mul_ov_128:
+	.hword	0
+	.hword	1
+	.hword	2
+	.hword	3
+	.hword	4
+	.hword	5
+	.hword	6
+	.hword	7
+	.hword	8
+	.hword	9
+	.hword	10
+	.hword	11
+	.hword	12
+	.hword	13
+	.hword	14
+	.hword	15
+	.hword	16
+	.hword	17
+	.hword	18
+	.hword	19
+	.hword	20
+	.hword	21
+	.hword	22
+	.hword	23
+	.hword	24
+	.hword	25
+	.hword	26
+	.hword	27
+	.hword	28
+	.hword	29
+	.hword	30
+	.hword	31
+	.hword	32
+	.hword	33
+	.hword	34
+	.hword	35
+	.hword	36
+	.hword	37
+	.hword	38
+	.hword	39
+	.hword	40
+	.hword	41
+	.hword	42
+	.hword	43
+	.hword	44
+	.hword	45
+	.hword	46
+	.hword	47
+	.hword	48
+	.hword	49
+	.hword	50
+	.hword	51
+	.hword	52
+	.hword	53
+	.hword	54
+	.hword	55
+	.hword	56
+	.hword	57
+	.hword	58
+	.hword	59
+	.hword	60
+	.hword	61
+	.hword	62
+	.hword	63
+	.hword	64
+	.hword	65
+	.hword	66
+	.hword	67
+	.hword	68
+	.hword	69
+	.hword	70
+	.hword	71
+	.hword	72
+	.hword	73
+	.hword	74
+	.hword	75
+	.hword	76
+	.hword	77
+	.hword	78
+	.hword	79
+	.hword	80
+	.hword	81
+	.hword	82
+	.hword	83
+	.hword	84
+	.hword	85
+	.hword	86
+	.hword	87
+	.hword	88
+	.hword	89
+	.hword	90
+	.hword	91
+	.hword	92
+	.hword	93
+	.hword	94
+	.hword	95
+	.hword	96
+	.hword	97
+	.hword	98
+	.hword	99
+	.hword	100
+	.hword	101
+	.hword	102
+	.hword	103
+	.hword	104
+	.hword	105
+	.hword	106
+	.hword	107
+	.hword	108
+	.hword	109
+	.hword	110
+	.hword	111
+	.hword	112
+	.hword	113
+	.hword	114
+	.hword	115
+	.hword	116
+	.hword	117
+	.hword	118
+	.hword	119
+	.hword	120
+	.hword	121
+	.hword	122
+	.hword	123
+	.hword	124
+	.hword	125
+	.hword	126
+	.hword	127
+	.hword	128
+	.hword	129
+	.hword	130
+	.hword	131
+	.hword	132
+	.hword	133
+	.hword	134
+	.hword	135
+	.hword	136
+	.hword	137
+	.hword	138
+	.hword	139
+	.hword	140
+	.hword	141
+	.hword	142
+	.hword	143
+	.hword	-1
+	.hword	9	// #TERMS(128,32)/32
+KA_col_128_ov:
+KA_col_ov_128_32:
+KA_col_add_128_32:
+	.hword	96	// =#shift/8, #iterations*4
+KA_col_ov_128_64:
+	.hword	16
+	.hword	47
+	.hword	80
+	.hword	111
+	.hword	144
+	.hword	175
+	.hword	-1
+KA_col_add_128_64:
+	.hword	64	// =#shift/8, #iterations*4
+
--- polymul_256x256_bar.h
+++ polymul_256x256_bar.h
@@ -1,1 +1,1 @@
-extern void gf_polymul_%dx%d(int32_t *h, int32_t *f, int32_t *g);
+extern void gf_polymul_256x256 (int32_t *h, int32_t *f, int32_t *g);
