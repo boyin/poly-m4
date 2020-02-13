6d85bdbb99faa5b862db922dcf02120d081d4b20
B.Y <by@crypto.tw>
2020-02-13 15:47:34 +0800

    aux files

---
+++ polymul_256x256_B32_aux.h
@@ -0,0 +1,175 @@
+	.p2align	2,,3
+	.syntax		unified
+	.text
+
+KA_exp_ov_256:
+KA_exp_ov_256_256:
+	.hword	-1
+KA_exp_add_256_256:
+	.hword	64	// #TERMS(256,256)/4
+KA_exp_ov_256_128:
+	.hword	-1
+KA_exp_add_256_128:
+	.hword	96	// #TERMS(256,128)/4
+KA_exp_ov_256_64:
+	.hword	-1
+KA_exp_add_256_64:
+	.hword	144	// #TERMS(256,64)/4
+KA_mul_ov_256:
+	.hword	256
+	.hword	257
+	.hword	258
+	.hword	259
+	.hword	260
+	.hword	261
+	.hword	262
+	.hword	263
+	.hword	264
+	.hword	265
+	.hword	266
+	.hword	267
+	.hword	268
+	.hword	269
+	.hword	270
+	.hword	271
+	.hword	272
+	.hword	273
+	.hword	274
+	.hword	275
+	.hword	276
+	.hword	277
+	.hword	278
+	.hword	279
+	.hword	280
+	.hword	281
+	.hword	282
+	.hword	283
+	.hword	284
+	.hword	285
+	.hword	286
+	.hword	287
+	.hword	352
+	.hword	353
+	.hword	354
+	.hword	355
+	.hword	356
+	.hword	357
+	.hword	358
+	.hword	359
+	.hword	360
+	.hword	361
+	.hword	362
+	.hword	363
+	.hword	364
+	.hword	365
+	.hword	366
+	.hword	367
+	.hword	368
+	.hword	369
+	.hword	370
+	.hword	371
+	.hword	372
+	.hword	373
+	.hword	374
+	.hword	375
+	.hword	376
+	.hword	377
+	.hword	378
+	.hword	379
+	.hword	380
+	.hword	381
+	.hword	382
+	.hword	383
+	.hword	384
+	.hword	385
+	.hword	386
+	.hword	387
+	.hword	388
+	.hword	389
+	.hword	390
+	.hword	391
+	.hword	392
+	.hword	393
+	.hword	394
+	.hword	395
+	.hword	396
+	.hword	397
+	.hword	398
+	.hword	399
+	.hword	400
+	.hword	401
+	.hword	402
+	.hword	403
+	.hword	404
+	.hword	405
+	.hword	406
+	.hword	407
+	.hword	408
+	.hword	409
+	.hword	410
+	.hword	411
+	.hword	412
+	.hword	413
+	.hword	414
+	.hword	415
+	.hword	416
+	.hword	417
+	.hword	418
+	.hword	419
+	.hword	420
+	.hword	421
+	.hword	422
+	.hword	423
+	.hword	424
+	.hword	425
+	.hword	426
+	.hword	427
+	.hword	428
+	.hword	429
+	.hword	430
+	.hword	431
+	.hword	-1
+	.hword	27	// #TERMS(256,32)/32
+KA_col_256_ov:
+KA_col_ov_256_32:
+KA_col_add_256_32:
+	.hword	288	// =#shift/8, #iterations*4
+KA_col_ov_256_64:
+	.hword	16
+	.hword	47
+	.hword	80
+	.hword	111
+	.hword	144
+	.hword	175
+	.hword	208
+	.hword	239
+	.hword	265
+	.hword	310
+	.hword	329
+	.hword	374
+	.hword	393
+	.hword	438
+	.hword	457
+	.hword	502
+	.hword	528
+	.hword	559
+	.hword	-1
+KA_col_add_256_64:
+	.hword	192	// =#shift/8, #iterations*4
+KA_col_ov_256_128:
+	.hword	32
+	.hword	60
+	.hword	66
+	.hword	94
+	.hword	160
+	.hword	188
+	.hword	194
+	.hword	222
+	.hword	288
+	.hword	316
+	.hword	322
+	.hword	350
+	.hword	-1
+KA_col_add_256_128:
+	.hword	128	// =#shift/8, #iterations*4
+
---
+++ polymul_64x64_bar_B32_aux.h
@@ -0,0 +1,65 @@
+	.p2align	2,,3
+	.syntax		unified
+	.text
+
+KA_exp_ov_64:
+KA_exp_ov_64_64:
+	.hword	-1
+KA_exp_add_64_64:
+	.hword	16	// #TERMS(64,64)/4
+KA_mul_ov_64:
+	.hword	0
+	.hword	1
+	.hword	2
+	.hword	3
+	.hword	4
+	.hword	5
+	.hword	6
+	.hword	7
+	.hword	8
+	.hword	9
+	.hword	10
+	.hword	11
+	.hword	12
+	.hword	13
+	.hword	14
+	.hword	15
+	.hword	16
+	.hword	17
+	.hword	18
+	.hword	19
+	.hword	20
+	.hword	21
+	.hword	22
+	.hword	23
+	.hword	24
+	.hword	25
+	.hword	26
+	.hword	27
+	.hword	28
+	.hword	29
+	.hword	30
+	.hword	31
+	.hword	32
+	.hword	33
+	.hword	34
+	.hword	35
+	.hword	36
+	.hword	37
+	.hword	38
+	.hword	39
+	.hword	40
+	.hword	41
+	.hword	42
+	.hword	43
+	.hword	44
+	.hword	45
+	.hword	46
+	.hword	47
+	.hword	-1
+	.hword	3	// #TERMS(64,32)/32
+KA_col_64_ov:
+KA_col_ov_64_32:
+KA_col_add_64_32:
+	.hword	32	// =#shift/8, #iterations*4
+
