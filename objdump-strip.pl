d7af60fea04d4fef144b933eae6b5ca4e36e7552
B.Y <by@crypto.tw>
2020-02-15 22:48:43 +0800

    stuff

--- objdump-strip.pl
+++ objdump-strip.pl
@@ -1,11 +1,12 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 
 use Classic::Perl;
 use Getopt::Std;
 
 getopts('n:g:');
 
-$last = ""; $lastop = ""; $lastbody="";
+my %labels;
+
 
 if ($opt_n) {
     @L = split(" ",$opt_n);
@@ -20,8 +21,8 @@ if ($opt_g) {
     }
 }
 
+$last = ""; $lastop = ""; $lastbody="";
 print " .p2align	2,,3\n .syntax		unified\n .text\n/*\n";
-
 while (<>) {
     if (/^Disassembly of section \.text:/) {print; print "*/\n"; last;
     } else {print;
@@ -29,6 +30,9 @@ while (<>) {
 }
 while (<>) {
     chomp;
+    #if (s/^([a-f0-9]{8}) <([A-Za-z0-9_]*)>:/$2:/g) {
+    #	$labels[$1]=$2;
+    #}
     s/^[a-f0-9]+ <([A-Za-z0-9_]*)>:/$1:/g;
     s/[a-f0-9]+ <([A-Za-z0-9_]*)>/$1/g;
     s/^\s*[a-f0-9]+:\s+[a-f0-9]{4}\s+[a-f0-9]{4}\s+([a-z]+(8|16)?)\s+/ $1.w /g;
@@ -36,22 +40,22 @@ while (<>) {
     s/^\s*[a-f0-9]+:\s+[a-f0-9]{4}\s+([a-z]+(8|16)?)\s+/ $1.n /g;
     s/^\s*[a-f0-9]+:\s+[a-f0-9]{4}\s+/ /g;
     s/; .*$//g;
-    if ($last) {
+    if ($last ne "") {
 	if ($lastop eq "bx") {
 	    print "$last\n nop.n\n";
 	    $last = ""; $lastop = ""; $lastbody="";
-	} elsif (/ ([a-z]+(8|16)?)\.n/) {
+	} elsif (/([a-z]+(8|16)?)\.n/) {
 	    print "$last\n$_\n"; 
 	    $last = ""; $lastop = ""; $lastbody=""; next;
 	} elsif ($lastop =~ /^it[et]{0,3}$/) {
 	    print " nop.n\n$last\n";
 	} else {
-	    ($last =~ s/ \.n/.w/);
+	    ($last =~ s/\.n/.w/);
 	    print "$last\n";
 	}
     }
-    if (/^ ([a-z]+(8|16)?)\.n(.*)$/) {
-	$last = $_; $lastop = $1; $lastbody=$2;
+    if (/^\s+([a-z]+(8|16)?)\.n(.*)$/) {
+	$last = $_; $lastop = $1; $lastbody=$3;
     } else {
 	print "$_\n"; $last=""; $lastop=""; $lastbody="";
     }
